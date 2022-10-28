e32c74e49d652bbb2cbbd43451ef70051fd07603
Emerson <96029393+Emersonpereiradossantos@users.noreply.github.com>
2022-10-28 16:07:47 -0300

    alunos

---
+++ OrdemNumericaDeAlunos.js
@@ -0,0 +1,11 @@
+let numeroDeAlunos = 10
+for (let contador = 0; contador < numeroDeAlunos; contador++){
+    //console.log(contador)
+    if (contador == 0){
+    console.log("o numero atual é zero")
+    }else if (contador % 2 == 0){
+        console.log(`o numero ${contador} é par`)
+    }else{
+        console.log(`o numero ${contador} é impar`)
+    }
+}
